4efb47627ea3fb0a85155a429b84e22853d51ee7
Samsher135 <12345samsingh.samu@gmail.com>
2021-09-08 11:42:06 +0530

    Initial Commit

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'blog'),
+        database: env('DATABASE_NAME', 'blog1'),
         username: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', ''),
       },
