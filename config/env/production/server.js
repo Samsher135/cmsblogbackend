4998734b6cd4b3c00a44a0d606172945215a8bd0
Samsher135 <12345samsingh.samu@gmail.com>
2021-09-09 14:32:25 +0530

    Update database config

---
+++ config/env/production/server.js
@@ -0,0 +1,4 @@
+module.exports = ({ env }) => ({
+    url: env('postgres://pupiceyfzhvbuc:27a1fbc8eb5fff3f557c8bb1b42b88e99d794e5765facc30cfbf1339a49dccf0@ec2-52-7-159-155.compute-1.amazonaws.com:5432/d338c93nnb5vm2'),
+  });
+  
